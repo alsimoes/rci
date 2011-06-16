b5785bfb6ff01693a5e6fc15e4cd437b1b7276e2
Andre Simoes <alsimoes@gmail.com>
2011-06-16 00:58:51 -0300

    remove branches and trunk directory

--- branches/test/setup.py
+++
@@ -1,5 +0,0 @@
-﻿from distutils.core import setup
-import py2exe
-
-setup(version = "0.1", name = "RCI", author = "Andre Simoes",data_files=[("rci.py")], console = ["rci.py"]  )
-
--- branches/test/teste.bat
+++
@@ -1,2 +0,0 @@
-rci.py "C:\Users\alsimoes\Dropbox\pos-graduacao\04-trabalhos\17-monografia_grupo4\Base de Conhecimento"
-pause
